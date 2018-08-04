2b3380cd43a40f7139efbb30222de70058a24124
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-04 18:55:56 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,10 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def connect 
-    self.artist.name + "-" + self.title 
+  def connect
+    self.artist.name + "-" + self.title
   end
+
+  def artist_name
+    self.artist.name
+  end 
